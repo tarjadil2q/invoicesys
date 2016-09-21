8bfd7aa71b7ccdb311681ce8728e580f861acbf7
tarjadil2q <github_1983>
2016-09-21 19:41:45 +1000

    fix mapping issue puk group to user

--- backend/src/main/java/com/pce/domain/User.java
+++ backend/src/main/java/com/pce/domain/User.java
@@ -42,7 +42,7 @@ public class User {
           inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
   private Set<Role> roles;
 
-  @ManyToMany(mappedBy = "usersInGroup")
+  @ManyToMany(mappedBy = "pukGroupUsers")
   private Set<PukGroup> pukGroups;
 
   @ManyToMany(mappedBy = "approvers")
